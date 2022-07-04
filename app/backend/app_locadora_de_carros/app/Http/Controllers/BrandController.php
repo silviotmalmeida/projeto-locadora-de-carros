179268b4660fa78e11ad22bee5e403f053f44900
silviotmalmeida <silviotmalmeida@gmail.com>
2022-07-04 18:03:20 -0300

    Implementacao ate aula 24

--- app/backend/app_locadora_de_carros/app/Http/Controllers/BrandController.php
+++ app/backend/app_locadora_de_carros/app/Http/Controllers/BrandController.php
@@ -8,24 +8,6 @@ use Illuminate\Http\Request;
 class BrandController extends Controller
 {
 
-    // definição das validações de cada campo
-    private $validationRules =
-    [
-        'name' => 'required|unique:brands|min:3|max:30',
-        'image' => 'required|min:3|max:100'
-    ];
-
-    // customização das mensagens de erro
-    private $validationMessages =
-    [
-        'required' => 'O campo não pode ser vazio!',
-        'name.unique' => 'O nome já está registrado no sistema',
-        'name.min' => 'O campo não pode ter menos de 3 caracteres!',
-        'name.max' => 'O campo não pode ter mais de 30 caracteres!',
-        'image.min' => 'O campo não pode ter menos de 3 caracteres!',
-        'image.max' => 'O campo não pode ter mais de 100 caracteres!',
-    ];
-
     // foi adicionado o construct para permitir a injeção do model
     /**
      * Create a new controller instance.
@@ -60,11 +42,22 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
+
         // validando os dados recebidos do formulário
-        $request->validate($this->validationRules, $this->validationMessages);
+        $request->validate($this->brand->rules(), $this->brand->feedback());
+
+        // obtendo os dados da imagem submetida na requisição
+        $img = $request->file('image');
+
+        // salvando o arquivo de imagem na pasta app/storage/app/public/images
+        // e obtendo o seu respectivo nome
+        $img_urn = $img->store('images', 'public');
 
         // insere os dados no BD
-        $brand = $this->brand->create($request->all());
+        $brand = $this->brand->create([
+            'name' => $request->name,
+            'image' => $img_urn
+        ]);
 
         // retornando o objeto criado e o status 201
         return response()->json($brand, 201);
@@ -82,7 +75,7 @@ class BrandController extends Controller
         $brand = $this->brand->find($id);
 
         // se a consulta não retornar nenhum registro, envia mensagem de erro 404
-        if($brand === null) return response()->json(['msg' => 'Recurso solicitado não existe.'], 404);
+        if ($brand === null) return response()->json(['msg' => 'Recurso solicitado não existe.'], 404);
 
         // retornando o objeto e o status 200
         return response()->json($brand, 200);
@@ -101,7 +94,33 @@ class BrandController extends Controller
         $brand = $this->brand->find($id);
 
         // se a consulta não retornar nenhum registro, envia mensagem de erro 404
-        if($brand === null) return response()->json(['msg' => 'Não foi possível realizar a atualização. Recurso solicitado não existe.'], 404);
+        if ($brand === null) return response()->json(['msg' => 'Não foi possível realizar a atualização. Recurso solicitado não existe.'], 404);
+
+        // se a requisição for PATCH, somente alguns atributos serão validados
+        if ($request->method() === 'PATCH') {
+
+            // inicializando o array de validações customizado
+            $validationRules = array();
+
+            // iterando sobre todas as regras definidas no model
+            foreach ($brand->rules($id) as $key => $value) {
+
+                // se o atributo da regra estiver presente no request, considera-o
+                if (array_key_exists($key, $request->all())) {
+
+                    $validationRules[$key] = $value;
+                }
+            }
+
+            // validando os dados recebidos do formulário
+            $request->validate($validationRules, $this->brand->feedback());
+        }
+        // se for PUT, valida todos os atributos
+        else {
+
+            // validando os dados recebidos do formulário
+            $request->validate($this->brand->rules($id), $this->brand->feedback());
+        }
 
         // atualiza os dados no BD
         $brand->update($request->all());
@@ -122,7 +141,7 @@ class BrandController extends Controller
         $brand = $this->brand->find($id);
 
         // se a consulta não retornar nenhum registro, envia mensagem de erro 404
-        if($brand === null) return response()->json(['msg' => 'Não foi possível realizar a exclusão. Recurso solicitado não existe.'], 404);
+        if ($brand === null) return response()->json(['msg' => 'Não foi possível realizar a exclusão. Recurso solicitado não existe.'], 404);
 
         // remove os dados no BD
         $brand->delete();
--- app/backend/app_locadora_de_carros/app/Models/Brand.php
+++ app/backend/app_locadora_de_carros/app/Models/Brand.php
@@ -23,4 +23,30 @@ class Brand extends Model
         'name',
         'image'
     ];
+
+    // definição das validações de cada campo
+    // recebe como arqumento o id do registro, se 
+    // este argumento será usado na restrição unique em caso de update
+    public function rules($id = null)
+    {
+        return [
+            'name' => 'required|unique:brands,name,' . $id . '|min:3',
+            'image' => 'required|file|mimes:png|min:3|max:100'
+        ];
+    }
+
+    // customização das mensagens de erro
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute não pode ser vazio!',
+            'name.unique' => 'O nome já está registrado no sistema',
+            'name.min' => 'O campo name não pode ter menos de 3 caracteres!',
+            'name.max' => 'O campo name não pode ter mais de 30 caracteres!',
+            'image.file' => 'O campo image precisa ser um arquivo!',
+            'image.mimes' => 'O campo image precisa ser uma imagem do tipo png!',
+            'image.min' => 'O campo image não pode ter menos de 3 caracteres!',
+            'image.max' => 'O campo image não pode ter mais de 100 caracteres!',
+        ];
+    }
 }
